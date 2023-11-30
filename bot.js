3664e5561a35ce8313280caf319a86a2ea4add78
ibonane <bonanedjimbala@gmail.com>
2023-11-30 13:09:52 +0100

    save old results into file json

--- bot.js
+++ bot.js
@@ -1,12 +1,57 @@
 const puppeteer = require('puppeteer');
 
-const criteria = {"nbCandidature": 5, "maxPrice": 600};
+const criteria = {"nbCandidature": 5, "maxPrice": 800, "reloadTime": 1};
 
 const fs = require('fs');
 // Lire le contenu du fichier JSON
 const jsonContent = fs.readFileSync('./secret.json', 'utf-8');
 // Parser le contenu JSON
 const secretData = JSON.parse(jsonContent);
+
+//===================== Block stocker les logements trouvés =======================//
+
+// Chemin du fichier où vous souhaitez sauvegarder les données
+const jsonFile = 'oldData.json';
+
+// Fonction pour sauvegarder le tableau dans un fichier
+function saveResultsToJson(array, oldAlreadyFound) {
+    // Convertir le array en chaîne JSON
+    oldArray = oldAlreadyFound.concat(array)
+    const tableJSON = JSON.stringify(array);
+
+    // Écrire la chaîne JSON dans le fichier
+    fs.writeFileSync(jsonFile, tableJSON);
+}
+
+// Fonction pour récupérer le tableau depuis le fichier
+function getOldResults() {
+    try {
+        // Lire le contenu du fichier
+        const jsonFileContent = fs.readFileSync(jsonFile, 'utf-8');
+
+        // Si le fichier est vide, retourner un nouveau tableau vide
+        if (!jsonFileContent) {
+            return [];
+        }
+
+        // Sinon, convertir la chaîne JSON en tableau et le retourner
+        return JSON.parse(jsonFileContent);
+    } catch (erreur) {
+        // En cas d'erreur (par exemple, si le fichier n'existe pas), retourner un nouveau tableau vide
+        return [];
+    }
+}
+
+function checkArrayTwoInArrayOne(array1, array2) {
+
+    const array1String = JSON.stringify(array1);
+    const array2String = JSON.stringify(array2);
+
+    // Vérifier si la chaîne JSON de array1 est incluse dans la chaîne JSON de a2
+    return array1String.includes(array2String) == true;
+}
+
+//===================== Block stocker les logements trouvés =======================//
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -25,6 +70,9 @@ function sendSMS(message) {
 }
 
 async function checkHomeAvailable() {
+    
+    var oldFound = getOldResults();
+    
     // 1. Lancer le navigateur
     console.log("Lancer le navigateur");
     const browser = await puppeteer.launch({headless: 'new'});
@@ -42,7 +90,7 @@ async function checkHomeAvailable() {
     console.log("se connecter");
     await page.click("#button-1012-btnInnerEl");
 
-    console.log("patienter ...");
+    console.log("Recherche en cours, patienter ...\n");
     await sleep(6000);
 
     // Utilisez page.evaluate pour exécuter une fonction JavaScript personnalisée dans le contexte de la page
@@ -52,7 +100,7 @@ async function checkHomeAvailable() {
         if (offersTab) {
             offersTab.click();
         } else {
-            console.error('Élément "Les offres" non trouvé');
+            console.error('\n<<<<<< Élément "Les offres" non trouvé >>>>>>>>\n');
         }
     });
     await sleep(5000);
@@ -77,19 +125,25 @@ async function checkHomeAvailable() {
 
         // Recherche de la valeur qui contient le symbole '€'
         const prixIndex = row.findIndex(value => value.includes('€'));
-
+        
         // Vérifier si le prix a été trouvé
         if (prixIndex !== -1) {
             // Extraire le montant du loyer en supprimant le symbole '€' et en le convertissant en entier
             loyer = parseInt(row[prixIndex].replace('€', ''), 10);
             
             if (nbCandidature < criteria.nbCandidature && loyer < criteria.maxPrice) {
-                homeFree.push(row);
+                //Si le logement trouvés a déjà été notifié, ne pas le rajouté
+                if(!checkArrayTwoInArrayOne(oldFound, row)){
+                    homeFree.push(row);
+                }
             }
         }
         else{
             if (nbCandidature < criteria.nbCandidature) {
-                homeFree.push(row);
+                //Si le logement trouvés a déjà été notifié, ne pas le rajouté
+                if(!checkArrayTwoInArrayOne(oldFound, row)){
+                    homeFree.push(row);
+                }
             }
         }
     });
@@ -97,20 +151,25 @@ async function checkHomeAvailable() {
 
     // 6. Envoyer un SMS si des logements sont disponibles
     if (homeFree.length > 0) {
-        console.log(homeFree.length + " logement(s) disponible(nt), envoi de sms en cours ...");
+
+        //Ajouter le logement trouvé
+        saveResultsToJson(homeFree, oldFound);
+
+        console.log("\n" + homeFree.length + " logement(s) disponible(nt), envoi de sms en cours ...");
 
         sendSMS("Des logements sont disponibles: " + homeFree.join(" ||| "));
 
-        console.log("sms envoyé avec succès au "+secretData.to);
+        console.log("sms envoyé avec succès au "+secretData.to + "\n");
     }
     else {
-        console.log("Aucun logement disponible");
+        console.log("\nRecherche terminée, Aucun logement disponible.\n");
     }
 
     await browser.close();
+    console.log("\nDéconnecter\n");
 }
 
-// On vérifie les logements une première fois puis toutes les 5 minutes
+// On vérifie les logements une première fois puis toutes les x minutes
 checkHomeAvailable();
-setInterval(checkHomeAvailable, 1000 * 60 * 5);
+setInterval(checkHomeAvailable, 1000 * 60 * criteria.reloadTime);
 
